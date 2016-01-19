2ee61abc1565668d140a386605a58773f52b1995
Yaroshevskyi <serhii.yaroshevskyi@gmail.com>
2016-01-19 21:52:35 +0200

    Implements article show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,24 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
-    
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+      redirect_to @article
+    else
+      render action: 'new'
+    end
   end
 
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)    
+  end
 end

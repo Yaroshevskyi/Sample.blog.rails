138805874ffc15a273565e0c3c21359fd76096c6
Yaroshevskyi <serhii.yaroshevskyi@gmail.com>
2016-01-19 22:55:06 +0200

    Implement articles edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+   def index
+    @articles = Article.all  
+  end
+
   def show
     @article = Article.find(params[:id])
   end
@@ -8,13 +12,27 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    if @article.valid?
-      @article.save
+    if @article.save
       redirect_to @article
     else
       render action: 'new'
     end
   end
+
+  def edit
+     @article = Article.find(params[:id])  
+  end
+
+  def update
+     @article = Article.find(params[:id]) 
+     if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'edit'
+    end
+
+  end
+
 
   private
 

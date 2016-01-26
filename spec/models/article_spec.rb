1df7a3b9c7242e122b808a0cc43fe0fcb3e57761
Yaroshevskyi <serhii.yaroshevskyi@gmail.com>
2016-01-26 14:02:55 +0200

    Add test for validation article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,13 @@
 require "spec_helper" 
 
 describe Article  do
-  it { should have_many :comments}
+  describe "validations" do
+    it { should validate_presence_of :title}
+    it { should validate_presence_of :text}
+  end  
+  
+  describe "assotiations" do
+  it { should have_many :comments}  
+  end
+
 end

3df296e3b5e0361d92d5f406057b70559c73552f
Yaroshevskyi <serhii.yaroshevskyi@gmail.com>
2016-01-27 23:28:23 +0200

    Add contacts acceptence test

---
+++ spec/features/visitor_create_contact_spec.rb
@@ -0,0 +1,8 @@
+require "spec_helper"
+
+feature "Contact Creation" do
+  scenario "allows acees to contacts page" do
+    visit '/contacts'
+    expect(page).to have_content 'Contact us'
+  end
+end

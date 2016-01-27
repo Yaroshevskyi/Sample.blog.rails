8b617cfbcc6db300ebc3e9e7adb06c288ecc8acd
Yaroshevskyi <serhii.yaroshevskyi@gmail.com>
2016-01-27 13:46:35 +0200

    Update article rspec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,4 +10,10 @@ describe Article  do
   it { should have_many :comments}  
   end
 
+  describe "#subject" do
+    it "returns the article title" do
+      article = create(:article, title: 'Lorem Ipsum')
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
+  end
 end

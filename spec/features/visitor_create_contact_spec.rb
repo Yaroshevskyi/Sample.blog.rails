825ebc4f45190615ff021ec535cb73db079cf258
Yaroshevskyi <serhii.yaroshevskyi@gmail.com>
2016-01-27 23:53:37 +0200

    Update create contact test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,6 +3,6 @@ require "spec_helper"
 feature "Contact Creation" do
   scenario "allows acees to contacts page" do
     visit '/contacts'
-    expect(page).to have_content 'Contact us'
+    expect(page).to have_content I18n.t 'contacts.contact_us'
   end
 end
